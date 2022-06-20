1691222f5e9e789937a4a3365e13328f11caf159
Jay Silesh <jayanthsileshs@gmail.com>
2022-06-19 22:57:19 -0700

    Add virtEnv, Requirements.txt dependency

--- src/server.py
+++ src/server.py
@@ -1,13 +1,16 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
+from urllib.parse import urlparse
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        query = urlparse(self.path).query
+        query_components = dict(qc.split("=") for qc in query.split("&"))
+        path = query_components["path"]
+
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-
-        message = "From new server"
+        message = "From new server the log path is /var/log/%s" % path
         self.wfile.write(bytes(message, "utf8"))
 
 
