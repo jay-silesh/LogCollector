fdf1dcb67bd598d26269a3473e05de6d1a28bf66
Jay Silesh <jayanthsileshs@gmail.com>
2022-06-21 10:57:43 -0700

    Add ability to start server with inputted port#

--- src/server.py
+++ src/server.py
@@ -1,4 +1,6 @@
+import getopt
 import logging
+import sys
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
 
@@ -10,14 +12,14 @@ from src.exceptions.server_error import ServerError
 from src.handlers import handle_request, handle_master_node_request
 from src.utils.http_utils import get_http_response_code
 
-PORT = 8000
+DEFAULT_PORT = 8000
 
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
             url_path = urlparse(self.path).path
-            if url_path == URL_LOGS_AGGREGATE: # TODO make this an interface!
+            if url_path == URL_LOGS_AGGREGATE:  # TODO make this an interface!
                 response: AggregateResponse = handle_master_node_request(self.path)
             elif url_path == URL_LOGS:
                 response: Response = handle_request(self.path)
@@ -44,6 +46,24 @@ class handler(BaseHTTPRequestHandler):
             self.send_response(get_http_response_code(e), "Unknown Error!")
 
 
-with HTTPServer(('', PORT), handler) as server:
-    print("Starting server on ", PORT)
-    server.serve_forever()
+def main(argv):
+    port = DEFAULT_PORT
+    try:
+        opts, args = getopt.getopt(argv, "hp:", ["port="])
+    except getopt.GetoptError:
+        print('server.py -p <port> -o <outputfile>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('server.py -p <port>')
+            sys.exit()
+        elif opt in ("-p", "--port"):
+            port = int(arg)
+    with HTTPServer(('', port), handler) as server:
+        print("Starting server on ", port)
+        server.serve_forever()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
